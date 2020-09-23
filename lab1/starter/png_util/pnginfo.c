fa032990b0e51cc5d18fbfc213c708f61a4a5cda
Hong Bin Jiang <hbjiang@uwaterloo.ca>
2020-09-22 23:03:59 -0400

    pnginfo changes

--- lab1/starter/png_util/pnginfo.c
+++ lab1/starter/png_util/pnginfo.c
@@ -9,6 +9,9 @@ int main(int argc, char** argv) {
 	if (fp == NULL) {
 		return -1;
 	}
+	
+	get_png_data_IHDR(buf, fp, 0, 0);
+	printf("%s: %u x %u", filename, buf->width, buf->height);
 	fclose(fp);
 	free(buf);
 	return 0;
