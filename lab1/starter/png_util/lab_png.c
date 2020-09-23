01f8f6607b6da36039fb251a41f779a10dab4d63
Hong Bin Jiang <hbjiang@uwaterloo.ca>
2020-09-22 20:18:47 -0400

    is_png finished

--- lab1/starter/png_util/lab_png.c
+++ lab1/starter/png_util/lab_png.c
@@ -3,6 +3,8 @@
 #include "lab_png.h"
 
 int is_png(U8 *buf, size_t n) {
+	printf("%d\n", *buf);
+	printf("%d\n", *(buf + 1));
 	//check the first 8 bytes
 	if (*buf != 0x89 ||
 		*(buf + 1) != 0x50 ||
@@ -14,7 +16,6 @@ int is_png(U8 *buf, size_t n) {
 		*(buf + 7) != 0x0a) {
 			return -1;
 	}
-	/*test test test*/
 	return 0;
 }
 
@@ -24,10 +25,6 @@ int get_png_data_IHDR(struct data_IHDR *out, FILE *fp, long offset, int whence) 
 
 /*unit tests*/
 int main () {
-	U8 test_buffer[8] = {0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x08};
-	U8 bad_buffer[8] = {0x89, 0x50, 0x4e, 0x47, 0x0d, 0x01, 0x1a, 0x04};
-	printf("%d", is_png(test_buffer, 0));
-	printf("%d", is_png(bad_buffer, 0));
 	return 0;
 }
 
