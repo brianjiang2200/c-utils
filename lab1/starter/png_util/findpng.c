0ae35a7eeaeb919a0c255c2c246685fcb7f234f4
Hong Bin Jiang <hbjiang@uwaterloo.ca>
2020-09-23 19:34:54 -0400

    findpng

--- lab1/starter/png_util/findpng.c
+++ lab1/starter/png_util/findpng.c
@@ -7,6 +7,38 @@
 #include <string.h>
 #include "lab_png.h"
 
+void check_directory(char* d_name, unsigned int* png_count) {
+	DIR* p_dir;
+	struct dirent *p_dirent;
+	char str[64];
+
+	/*if directory cannot be opened, simply return*/
+	if ((p_dir = opendir(d_name)) == NULL) {
+		return;
+	}
+
+	while ((p_dirent = readdir(p_dir)) != NULL) {
+		char *str_path = p_dirent->d_name;
+
+		if (str_path == NULL) {
+			return;
+		} else {
+			if (p_dirent->d_type == 4) {
+                                /*check directories are not "." and ".."*/
+                                if (strcmp(str_path, ".") != 0 && strcmp(str_path, "..") != 0) {                                                         /*recursively check subfolders*/
+                                }
+                        }
+                        /*else if potential png candidate found*/
+                        if (p_dirent->d_type == 8) {
+                                /*check first if valid png*/
+                                printf("%s\n", str_path);
+                                png_count++;
+                        }
+		}
+	}
+
+}
+
 int main(int argc, char** argv) {
 	DIR *p_dir;
 	struct dirent *p_dirent;
@@ -23,15 +55,34 @@ int main(int argc, char** argv) {
 		exit(2);
 	}
 
+	unsigned int png_count = 0;
+
+	/*keep reading directory contents*/
 	while ((p_dirent = readdir(p_dir)) != NULL) {
 		char *str_path = p_dirent->d_name;
 
 		if (str_path == NULL) {
-			fprintf(stderr, "Null pointer found!");
 			exit(3);
 		} else {
-			printf("%s\n", str_path);
+			/*if directory is found*/
+			if (p_dirent->d_type == 4) {
+				/*check directories are not "." and ".."*/
+				if (strcmp(str_path, ".") != 0 && strcmp(str_path, "..") != 0) {
+					/*recursively check subfolders*/
+				}
+			}
+			/*else if potential png candidate found*/
+			if (p_dirent->d_type == 8) {
+				/*check first if valid png*/
+				printf("%s\n", str_path);
+				png_count++;
+			}
 		}
+	}
+
+	/*if empty search result*/
+	if (!png_count) {
+		printf("findpng: No PNG file found\n");
 	}
 
 	if (closedir(p_dir) != 0) {
