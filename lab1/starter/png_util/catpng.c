6c075bbe1762c8d34838e0cbe2eb6d543eebe4df
Hong Bin Jiang <hbjiang@uwaterloo.ca>
2020-09-23 16:02:30 -0400

    findpng test

--- lab1/starter/png_util/catpng.c
+++ lab1/starter/png_util/catpng.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lab_png.h"
-#include "crc.h"
 
 int main(int argc, char** argv) {
 	return 0;
--- lab1/starter/png_util/findpng.c
+++ lab1/starter/png_util/findpng.c
@@ -1,9 +1,43 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "lab_png.h"
-#include "crc.h"
 
 int main(int argc, char** argv) {
+	DIR *p_dir;
+	struct dirent *p_dirent;
+	char str[64];
+
+	if (argc == 1) {
+		fprintf(stderr, "Usage: %s <directory name>\n", argv[0]);
+		exit(1);
+	}
+
+	if ((p_dir = opendir(argv[1])) == NULL) {
+		sprintf(str, "opendir(%s)", argv[1]);
+		perror(str);
+		exit(2);
+	}
+
+	while ((p_dirent = readdir(p_dir)) != NULL) {
+		char *str_path = p_dirent->d_name;
+
+		if (str_path == NULL) {
+			fprintf(stderr, "Null pointer found!");
+			exit(3);
+		} else {
+			printf("%s\n", str_path);
+		}
+	}
+
+	if (closedir(p_dir) != 0) {
+		perror("closedir");
+		exit(3);
+	}
+
 	return 0;
 }
