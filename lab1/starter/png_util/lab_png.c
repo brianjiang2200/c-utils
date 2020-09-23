17a6a30acf7dd69406d0dcf54098d9d338a58a22
t48shen <t48shen@uwaterloo.ca>
2020-09-22 21:04:15 -0400

    wrote get_data_IHDR function

--- lab1/starter/png_util/lab_png.c
+++ lab1/starter/png_util/lab_png.c
@@ -20,15 +20,26 @@ int is_png(U8 *buf, size_t n) {
 }
 
 int get_png_data_IHDR(struct data_IHDR *out, FILE *fp, long offset, int whence) {
+
+	if(fp == NULL) {
+		return -1;
+	}
+
+	fseek(fp, 16, SEEK_SET);
+
+	fread(&buf->width, sizeof(U32), 1, fp);
+
+	fread(&buf->height, sizeof(U32), 1, fp);
+
 	return 0;
+}
+
+U32 get_png_width(struct data_IHDR *buf) {
+        return buf->width;
 }
 
 U32 get_png_height(struct data_IHDR *buf) {
 	return buf->height;
-}
-
-U32 get_png_width(struct data_IHDR *buf) {
-	return buf->width;
 }
 
 /*unit tests*/
@@ -36,5 +47,3 @@ int main () {
 	return 0;
 }
 
-
-/*test*/
