c344628f10e743e082c699fdcf75180c691f9879
Hong Bin Jiang <hbjiang@uwaterloo.ca>
2020-10-11 19:04:21 -0400

    optarg

--- lab2/starter/paster.c
+++ lab2/starter/paster.c
@@ -1,7 +1,27 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
+#include <unistd.h>
 #include <curl/curl.h>
+#include <getopt.h>
 
 int main(int argc, char** argv) {
+	int c;
+	int no_threads = 1;
+	int img_no = 1;
+	while ((c = getopt(argc, argv, "t:n:")) != -1) {
+		switch(c) {
+		case 't':
+			no_threads = strtoul(optarg, NULL, 10);
+			break;
+		case 'n':
+			img_no = strtoul(optarg, NULL, 10);
+			break;
+		default:
+			break;
+		}
+	}
+	printf("%d %d\n", no_threads, img_no);
 	return 0;
 }
