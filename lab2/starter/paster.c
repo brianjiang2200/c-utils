ed44ad9928bc0321f53ea6eba4a38df28f6365c4
Hong Bin Jiang <hbjiang@uwaterloo.ca>
2020-10-12 14:49:16 -0400

    trying cURL

--- lab2/starter/paster.c
+++ lab2/starter/paster.c
@@ -7,6 +7,8 @@
 #include <getopt.h>
 #include "catpng.h"
 
+#define IMG_URL "http://ece252-1.uwaterloo.ca:2520/image?img="
+
 int main(int argc, char** argv) {
 	int c;
 	int no_threads = 1;
@@ -26,8 +28,24 @@ int main(int argc, char** argv) {
 		}
 	}
 
-	FILE* merged = fopen("all.png", "w");
-	fclose(merged);
+	/*setup CURL*/
+	CURL *curl_handle;
+	CURLcode res;
+	char url[256];
+	sprintf(url, "%s%d", IMG_URL, img_no);
+
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	curl_handle = curl_easy_init();
+	if (curl_handle) {
+		curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+		res = curl_easy_perform(curl_handle);
+		if (res != CURLE_OK) {
+			printf("curl_easy_perform() failed");
+		}
+		curl_easy_cleanup(curl_handle);
+	}
+
+	curl_global_cleanup();
 
 	return 0;
 }
