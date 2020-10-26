471b61415cef0acffffc3caec8383b89e20f2ed0
Hong Bin Jiang <hbjiang@uwaterloo.ca>
2020-10-26 14:13:37 -0400

    slaving away ;(

--- lab3/starter/paster2.c
+++ lab3/starter/paster2.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -14,6 +13,9 @@
 
 #define IMG_URL "http://ece252-1.uwaterloo.ca:2530/image?img="
 #define ECE252_HEADER "X-Ece252-Fragment: "
+
+int consumer();
+int producer();
 
 int main(int argc, char** argv) {
 
@@ -45,7 +47,40 @@ int main(int argc, char** argv) {
 	Buffer_init(global_buf, buf_size);
 
 	/*Do work here*/
+	/*Initialize producer processes*/
+	pid_t* prod_ids = malloc(no_producers * sizeof(pid_t));
+	for (int i = 0; i < no_producers; ++i) {
+		if ((prod_ids[i] = fork()) < 0) {
+			perror("fork");
+			abort();
+		} else if (prod_ids[i] == 0) {
+			/*perform all producer work here*/
+			printf("Producer working!\n");
+			return 0;
+		}
+	}
+	/*Initialize consumer processes*/
+	pid_t* cons_ids = malloc(no_consumers * sizeof(pid_t));
+	for (int i = 0; i < no_consumers; ++i) {
+		if ((cons_ids[i] = fork()) < 0) {
+			perror("fork");
+			abort();
+		} else if (cons_ids[i] == 0) {
+			/*perform all consumer work here*/
+			printf("Consumer working!\n");
+			return 0;
+		}
+	}
 
+	/*wait for all child processes to finish*/
+	int status;
+	pid_t pid;
+	for (int i = 0; i < no_producers; ++i) {
+		pid = wait(&status);
+	}
+	for (int i = 0; i < no_consumers; ++i) {
+		pid = wait(&status);
+	}
 
 	/*Initialize all.png chunks after work has been performed*/
 
@@ -58,6 +93,8 @@ int main(int argc, char** argv) {
 	times[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
 	printf("paster2 execution time: %.6lf seconds\n", times[1] - times[0]);
 
+	free(prod_ids);
+	free(cons_ids);
 	free(IDAT_arr);
 	Buffer_clean(global_buf);
 	free(global_buf);
