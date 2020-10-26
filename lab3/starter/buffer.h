24ce1681ac726b0c0030d91c6d2dd3be8f37de80
Hong Bin Jiang <hbjiang@uwaterloo.ca>
2020-10-25 20:56:20 -0400

    Buffer method defns

--- lab3/starter/buffer.h
+++ lab3/starter/buffer.h
@@ -2,4 +2,16 @@
 #include <stdlib.h>
 #include "main_write_header_cb.h"
 
-typedef struct 
+typedef struct Buffer
+{
+	int size;
+	RECV_BUF* tail;
+	RECV_BUF* head;
+} Buffer;
+
+void Buffer_init();
+void Buffer_add(RECV_BUF* node);
+void Buffer_pop();
+
+
+
