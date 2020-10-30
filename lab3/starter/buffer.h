1ea3b6b403adbb3a5eb4744f629ef27c9ea1849a
Hong Bin Jiang <hbjiang@uwaterloo.ca>
2020-10-29 23:31:52 -0400

    changes

--- lab3/starter/buffer.h
+++ lab3/starter/buffer.h
@@ -16,7 +16,7 @@ void Buffer_init(Buffer* b, int max_size);
 void Buffer_add(Buffer* b, RECV_BUF* node);
 void Buffer_pop(Buffer* b);
 void Buffer_clean(Buffer *b);
-int sizeof_Buffer(size_t recv_buf_size);
+int sizeof_Buffer(int buffer_size, size_t recv_buf_size);
 
 
 
