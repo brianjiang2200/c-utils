88911b73c60c8ffd6d0aa1f8614f56a32bab8942
Hong Bin Jiang <hbjiang@uwaterloo.ca>
2020-11-08 21:45:00 -0500

    initial commit

---
+++ lab4/starter/findpng2.c
@@ -0,0 +1,38 @@
+#define _DEFAULT_SOURCE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <curl/curl.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+int main(int argc, char** argv) {
+
+	double times[2];
+	struct timeval tv;
+
+	/*record time before first process*/
+	if (gettimeofday(&tv, NULL) != 0) {
+		perror("gettimeofday");
+		abort();
+	}
+	times[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
+
+	/*record time after program execution is finished*/
+	if (gettimeofday(&tv, NULL) != 0) {
+		perror("gettimeofday");
+		abort();
+	}
+	times[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
+	printf("findpng2 execution time: %.6lf seconds\n", times[1] - times[0]);
+
+	return 0;
+}
+
