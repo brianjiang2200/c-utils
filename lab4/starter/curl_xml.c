6c630c4c195977eb43937e5e479b133b8291f8c9
t48shen <t48shen@uwaterloo.ca>
2020-11-15 23:49:50 -0500

    adding all local files

--- lab4/starter/curl_xml.c
+++ lab4/starter/curl_xml.c
@@ -417,7 +417,7 @@ int process_png(CURL *curl_handle, RECV_BUF *p_recv_buf, void* arg)
 				p_in->phead = new_node;
 				(*(p_in->pngs_collected))++;
 
-				printf("PNG COUNT: %d\n", *p_in->pngs_collected);
+//TEST				printf("PNG COUNT: %d\n", *p_in->pngs_collected);
 
 			} else {
 				free(new_node->url);
